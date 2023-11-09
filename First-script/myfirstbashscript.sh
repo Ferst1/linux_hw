8039598df0a46c62b734c40d075b9113220ab173
ElenaK <helenochka4@gmail.com>
2023-11-09 19:22:08 +0100

    file.sh

---
+++ First-script/myfirstbashscript.sh
@@ -0,0 +1,11 @@
+USER="KasprukE"
+CURRENT_DATE=$(date +"%Y-%m-%d %H:%M:%S")
+echo "Текущая дата: $CURRENT_DATE"
+echo "Привет, $USER!"
+CURRENT_DIRECTORY=$(pwd)
+echo "Сейчас я работаю из директории: $CURRENT_DIRECTORY"
+chmod +x myfirstbashscript.sh
+
+
+
+
